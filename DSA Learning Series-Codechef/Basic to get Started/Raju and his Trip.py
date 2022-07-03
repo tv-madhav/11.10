96be9ea6d7fb9e3e4080496b3040405eb9cdeff3
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2022-07-03 21:26:32 +0530

    Add files via upload

---
+++ DSA Learning Series-Codechef/Basic to get Started/Raju and his Trip.py
@@ -0,0 +1,28 @@
+Raju is planning to visit his favourite restaurant. He shall travel to it by bus. Only the buses whose numbers are divisible by 5 or by 6 shall take him to his destination. You are given a bus number N. Find if Raju can take the bus or not. Print YES if he can take the bus, otherwise print NO.
+
+Input:
+The first and only line of the input shall contain an integer N, denoting the bus number.
+Output:
+Print YES if Raju can take that bus, else print NO.
+
+Constraints
+1≤N≤106
+Sample Input 1:
+60
+Sample Output 1:
+YES
+Sample Input 2:
+16
+Sample Output 2:
+NO
+
+Solution:
+#tvm
+
+N = int(input())
+if (N%5==0  or N%6==0 ):
+    print("YES")
+else:
+    print("NO")
+    
+#
